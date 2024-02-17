943d5b4b9f53f59c7487eebeea37608bd65ee72d
naqeebahmad212 <naqeebahmad212@gmail.com>
2024-02-17 14:17:03 +0500

    sidebarU

--- components/SideTest.tsx
+++ components/SideTest.tsx
@@ -60,7 +60,7 @@ const SideTest = () => {
                       fill="currentColor"
                     >
                       <path
-                        className={`fill-current text-gray-300 group-hover:text-cyan-300 ${pathname === "/admin/all-categories" ? "text-cyan-300" : "text-gray-600"}`}
+                        className={`fill-current group-hover:text-cyan-300 ${pathname === "/admin/all-categories" ? "text-cyan-300" : "text-gray-600"}`}
                         fill-rule="evenodd"
                         d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
                         clip-rule="evenodd"
@@ -87,13 +87,13 @@ const SideTest = () => {
                       fill="currentColor"
                     >
                       <path
-                        className={`fill-current text-gray-600 group-hover:text-cyan-600 ${pathname === "/admin/all-posts" ? "text-cyan-600" : "text-gray-600"}`}
+                        className={`fill-current group-hover:text-cyan-600 ${pathname === "/admin/all-posts" ? "text-cyan-600" : "text-gray-600"}`}
                         fill-rule="evenodd"
                         d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
                         clip-rule="evenodd"
                       />
                       <path
-                        className={`fill-current text-gray-300 group-hover:text-cyan-300 ${pathname === "/admin/all-posts" ? "text-cyan-300" : "text-gray-300"}`}
+                        className={`fill-current group-hover:text-cyan-300 ${pathname === "/admin/all-posts" ? "text-cyan-300" : "text-gray-300"}`}
                         d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"
                       />
                     </svg>
@@ -107,7 +107,7 @@ const SideTest = () => {
                   >
                     <PeopleIcon
                       fontSize="small"
-                      className={`fill-current text-gray-600 group-hover:text-cyan-300 ${pathname === "/admin/all-users" ? "text-cyan-300" : "text-gray-600"}`}
+                      className={`fill-current group-hover:text-cyan-300 ${pathname === "/admin/all-users" ? "text-cyan-300" : "text-gray-600"}`}
                     />
                     <span className="group-hover:text-gray-700">Users</span>
                   </Link>
