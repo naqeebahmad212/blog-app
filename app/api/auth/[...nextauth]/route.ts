80267347c75c260ccdca03e10112ed8bbd88e235
Naqeeb <naqeebahmad212@gmail.com>
2024-04-06 01:11:08 +0500

    test

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,6 @@
 // import { mergAnonymusCart } from "@/lib/db/cart";
 import { authOptions } from "@/lib/auth";
-import { prisma } from "@/lib/db/prisma";
-import { env } from "@/lib/env";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-import { NextAuthOptions } from "next-auth";
-import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-
-
 
 const handler = NextAuth(authOptions);
 
--- components/ui/tracing-beam.tsx
+++ components/ui/tracing-beam.tsx
@@ -27,18 +27,18 @@ export const TracingBeam = ({
   console.log(scrollYProgress);
   return (
     <>
-      <div className="fixed top-[20%] left-2 md:left-5 w-1 h-[230px] bg-gray-300 rounded-2xl">
+      <div className="fixed top-[20%] left-2 md:left-5 w-1 h-[50dvh] bg-gray-300 rounded-2xl">
         {scrollYProgress.get() == 0 && (
-          <span className="absolute -left-[2px] top-0 h-1 w-1 progressBar p-1 rounded-full"></span>
+          <span className="absolute -left-[2px] -top-[4px] h-1 w-1 bg-blue-400 p-1 rounded-full"></span>
         )}
       </div>
       <motion.div
-        className="fixed top-[20%] left-2 md:left-5 w-1 h-[230px] progressBar rounded-full overflow-hidden"
+        className="fixed top-[20%] left-2 md:left-5 w-1 h-[50dvh] progressBar rounded-3xl "
         style={{
           scaleY: scrollYProgress,
           transformOrigin: "top",
         }}
-        transition={{ duration: 0.2, type: "spring" }}
+        transition={{ duration: 0.5, type: "spring" }}
       ></motion.div>
       <div className="" ref={contentRef}>
         {children}
